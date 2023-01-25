48bb18ff90a45be8494a0c3e403714ac1af5564f
Zubair <zubair.maqsood6@gmail.com>
2023-01-25 04:26:47 +0000

    changing firebase.ts to env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPNWwpJ5fa1mEAmsNlgXYks6EpIEnH9Fs",
-  authDomain: "nn-design.firebaseapp.com",
-  projectId: "nn-design",
-  storageBucket: "nn-design.appspot.com",
-  messagingSenderId: "232898015650",
-  appId: "1:232898015650:web:422812c472076ed1edc2fb",
-  measurementId: "G-E3GPH3JEB4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN as string,
+  projectId: process.env.REACT_APP_PROJECT_ID as string,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID as string,
+  appId: process.env.REACT_APP_ID as string,
+  measurementId: process.env.REACT_MEASUREMENT_ID as string
 };
 
 // Initialize Firebase
